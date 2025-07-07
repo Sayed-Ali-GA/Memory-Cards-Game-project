2758895f36a26879ca7ff11de8cbb36d9d833a26
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-07-08 02:16:54 +0300

    Add timer and message with different colors, and add hover effect for cards

--- js/app.js
+++ js/app.js
@@ -2,39 +2,38 @@
 
 
 /*---------- Variables (state) ---------*/
-let matched = 0;
 let cardOne =null
 let cardTwo = null
 let disableDeck = false;
-let sec = 30;
+let sec = 20;
 let score = 0;
-let msg
 /*----- Cached Element References  -----*/
 const cards = document.querySelectorAll('.card')
 const resetBtn = document.getElementById('reset')
 const scoreDisplay = document.querySelector('.score')
 const timerDisplay = document.querySelector('.timer')
+const messageEl = document.querySelector('.message')
+const msgaLo = document.querySelector('.msgLos')
 
 /*--------------- Functions -------------*/
+
+function startTimer() {
+  timer = setInterval(() => {
+    if (sec > 0 ) {
+      sec--;
+      timerDisplay.textContent = `Timer:00:${sec < 10 ? '0' + sec: sec}`;
+} else {
+      clearInterval(timer);
+      disableDeck = true;
+      msgaLo.textContent = `You lose! Time's up`;
+    }
+  }, 1000);
+}
+
  function render(){
     scoreDisplay.textContent = `Score: ${score}`
     timerDisplay.textContent = `Timer:00:${sec}`
- }
-
-if(score === 6){
-  
-}
-
-function timeraa(){ 
-    if(flipCard){ 
-    
-        sec--;
-        }
-          timerDisplay.textContent = `Timer: ${sec}`
-    }
-    
-    
-     
+ } 
 
  function flipCard(card){
     if(card === cardOne || disableDeck) return
@@ -42,61 +41,59 @@ function timeraa(){
     if(!cardOne){
         cardOne = card
         return
-    }
+  }
     cardTwo = card
     disableDeck = true
     checkMatch();
  }
 
-
    function checkMatch() {
      const id1 = cardOne.querySelector('.card-back').id
      const id2 = cardTwo.querySelector('.card-back').id
        if (id1 === id2) {
-             matched++;
-             score++;
-              cardOne = null;
-               cardTwo = null;
-                disableDeck = false;
-  } else {
-    setTimeout(() => {
-        cardOne.classList.remove('flipped');
-          cardTwo.classList.remove('flipped');
+            score++;
             cardOne = null;
-              cardTwo = null;
-               disableDeck = false;
+            cardTwo = null;
+            disableDeck = false;
+
+       if(score === 6){
+         clearInterval(timer);
+         disableDeck = true
+         messageEl.textContent = `You win in ${sec}s `
+ } 
+} else {
+     setTimeout(() => {
+       cardOne.classList.remove('flipped');
+       cardTwo.classList.remove('flipped');
+        cardOne = null;
+        cardTwo = null;
+        disableDeck = false;
     }, 1000);
   }
-render();
-}
+   render();
+ }
 
 
-resetBtn.addEventListener('click', () => {
-  cards.forEach(card => {
-    card.classList.remove('flipped');
-    score = 0
-    scoreDisplay.textContent = null
-           scoreDisplay.textContent = `Score: 0`
-  })})
+   resetBtn.addEventListener('click', () => {
+     cards.forEach(card => {
+       card.classList.remove('flipped');
+       sec = 20 // for timer begain 20s
+       score = 0 
+     scoreDisplay.textContent = null
+        scoreDisplay.textContent = `Score: 0`
+        timerDisplay.textContent = null
+        timerDisplay.textContent = `Timer:00:00 `
+        disableDeck = false; 
+        messageEl.textContent = ''
+        msgaLo.textContent = ''       
+    })   
+    startTimer() 
+ });          
+
+  startTimer() 
 
 cards.forEach(card => {
   card.addEventListener('click', () => flipCard(card));
 });
-/*------------------------------------------------------*/
-
 
 /*----------- Event Listeners ----------*/
-resetBtn.addEventListener('click',resetBtn)
-
-
-
-
-
-
-
-
-
-/*---------------------------------------------------------*/
-// cardOne = card.children[0].children[1].id 
-//  const id1 = cardOne.querySelector('.card-back')
-//   const id2 = cardTwo.querySelector('.card-back')
